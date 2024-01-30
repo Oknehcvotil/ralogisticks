099df30f7461485fa8c986272c4aa8468627c248
Kirill <litovchenko.br@gmail.com>
2024-01-31 02:54:48 +0300

    Router
    removed basename

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate persistor={persistor}>
-        <BrowserRouter basename="/ralogisticks">
+        <BrowserRouter>
           <App />
         </BrowserRouter>
       </PersistGate>
