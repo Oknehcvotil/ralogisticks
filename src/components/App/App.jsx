aafc10dcf838ddfd81357fc364fb938fd3225b2b
Kirill <litovchenko.br@gmail.com>
2024-01-31 03:57:16 +0300

    Services page
    add Suspense and Outlet

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,7 +4,6 @@ import HomePage from 'pages/HomePage/HomePage';
 import AboutUsPage from 'pages/AboutUsPage/AboutUsPage';
 import ServicesPage from 'pages/ServicesPage/ServicesPage';
 import ContactsPage from 'pages/ContactsPage/Contacts';
-// import DefaultLanguageRoute from 'components/DefaultLanguageRoute/DefaultLanguageRoute';
 import InternationalTransportationPage from 'pages/InternationalTransportationPage/InternationalShippingPage';
 import SeaShippingPage from 'pages/SeaShippingPage/SeaShippingPage';
 import MultimodalTransportationPage from 'pages/MultimodalTransportationPage/MultimodalTransportationPage';
@@ -17,29 +16,26 @@ const App = () => {
   return (
     <Routes>
       <Route path="/" element={<DefaultLanguageRoute defaultLanguage="ua" />} />
-        <Route path=":language" element={<Layout />}>
-          <Route index element={<HomePage />} />
-          <Route path="about-us" element={<AboutUsPage />} />
-          <Route path="services" element={<ServicesPage />}>
-            <Route
-              path="international-transportation"
-              element={<InternationalTransportationPage />}
-            />
-            <Route path="sea-shipping" element={<SeaShippingPage />} />
-            <Route
-              path="multimodal-transportation"
-              element={<MultimodalTransportationPage />}
-            />
-            <Route
-              path="customs-clearance"
-              element={<CustomsCLearancePage />}
-            />
-            <Route path="cargo-insurance" element={<CargoInsurancePage />} />
-          </Route>
-          <Route path="contacts" element={<ContactsPage />} />
+      <Route path="/:language" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path="about-us" element={<AboutUsPage />} />
+        <Route path="services" element={<ServicesPage />}>
+          <Route
+            path="international-transportation"
+            element={<InternationalTransportationPage />}
+          />
+          <Route path="sea-shipping" element={<SeaShippingPage />} />
+          <Route
+            path="multimodal-transportation"
+            element={<MultimodalTransportationPage />}
+          />
+          <Route path="customs-clearance" element={<CustomsCLearancePage />} />
+          <Route path="cargo-insurance" element={<CargoInsurancePage />} />
         </Route>
-        <Route path="404" element={<NotFoundPage />} />
-        <Route path="*" element={<Navigate to="/404" />} />
+        <Route path="contacts" element={<ContactsPage />} />
+      </Route>
+      <Route path="404" element={<NotFoundPage />} />
+      <Route path="*" element={<Navigate to="/404" />} />
     </Routes>
   );
 };
--- src/pages/ServicesPage/ServicesPage.jsx
+++ src/pages/ServicesPage/ServicesPage.jsx
@@ -1,5 +1,15 @@
+import { Outlet } from 'react-router-dom';
+import { Suspense } from 'react';
+
 const ServicesPage = () => {
-    return <h2>ServicesPage</h2>;
-}
+  return (
+    <>
+      <h2>ServicesPage</h2>
+      <Suspense fallback={'loadin...'}>
+        <Outlet />
+      </Suspense>
+    </>
+  );
+};
 
 export default ServicesPage;
