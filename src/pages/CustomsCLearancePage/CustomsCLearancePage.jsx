cf908b5ed81ae6f19e57e4ae95d60b36f8aa9244
Kirill <litovchenko.br@gmail.com>
2024-02-06 21:48:56 +0300

    Custom clearance page
    add and complited custom clearance page

--- src/pages/CustomsCLearancePage/CustomsCLearancePage.jsx
+++ src/pages/CustomsCLearancePage/CustomsCLearancePage.jsx
@@ -1,5 +1,21 @@
+import ServicesPageSection from 'components/ServicesPageSection/ServicesPageSection';
+
 const CustomsCLearancePage = () => {
-  return <h2>CustomsCLearancePage</h2>;
+  const paragraphs = [
+    'pages.services.customsService.firstParagraf',
+    'pages.services.customsService.secondParagraf',
+  ];
+
+  return (
+    <section>
+      <ServicesPageSection
+        altText={'Customs Clearance'}
+        imagePath={'customs_service/customs'}
+        title={'navigation.services.customs'}
+        paragraphs={paragraphs}
+      />
+    </section>
+  );
 };
 
 export default CustomsCLearancePage;
