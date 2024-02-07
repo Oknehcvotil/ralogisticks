d0253c1b663f7f3686c51bc96fdbb39ab0bfe57d
Kirill <121458409+Oknehcvotil@users.noreply.github.com>
2024-02-07 18:08:26 +0200

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -40,7 +40,7 @@ const ContactForm = ({ className }) => {
     try {
       // Отправка данных на бэкенд
       const response = await axios.post(
-        'http://ralogisticks-backend.vercel.app/submit-form',
+        'https://ralogistics.onrender.com/submit-form',
         values
       );
 
