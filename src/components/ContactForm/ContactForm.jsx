8014c0cf368582bbbbf4f1655e54608feac91c52
Kirill <121458409+Oknehcvotil@users.noreply.github.com>
2024-02-08 20:45:30 +0200

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import axios from 'axios'; 
+import axios from 'axios';
 import CustomInput from 'components/CustomInput/CustomInput';
 import { Formik, Field } from 'formik';
 import { FormStyled, InputContainer } from './ContactForm.styled';
